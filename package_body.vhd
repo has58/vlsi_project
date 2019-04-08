69cda3897cdb1b9868ddde35113fad80f8bda041
has58 <has58@njit.edu>
2019-04-08 19:02:29 -0400

    all package perfetcly working

--- package_body.vhd
+++ package_body.vhd
@@ -14,12 +14,15 @@
 -- $Package : 1 $
 -- $Date: 2019-04-04 09:48:09 (WED, 4 Apr 2019) $
 -- --------------------------------------------------------------------
+library IEEE;
+use ieee.std_logic_1164.ALL;
+library WORK;
 package body Network_Coding is
 	use WORK.Network_Coding.ALL;
-  function encoding (input1,input2: std_logic_vector)
-	return std_logic_vector is
+  	function encoding (signal input1,input2: std_logic)
+	return std_logic is
 	begin
 		return (input1 XOR input2);
-    	end encoding;
+    	end function encoding;
 
   end Network_Coding;
